8c6aaa8f1c9d514fab672f5eba87a7e247c05644
864722370 <15626138260@163.com>
2019-09-06 16:59:03 +0800

    接入搜索接口

--- pages/search/index.js
+++ pages/search/index.js
@@ -12,7 +12,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    s.get("", {}, function (t) {
+    s.get("merch/select_cate_get_goods", {
+      'keyword':'丰田'
+    }, function (t) {
       
     });
   },
