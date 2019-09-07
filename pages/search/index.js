85ff60ade7c79da61356e0cfef811597ff92bb6d
limengge1314 <15626138260@163.com>
2019-09-07 16:18:48 +0800

    xiu

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,92 +1,104 @@
-var a, e, i = getApp(), s = i.requirejs("core");
+var a, e, i = getApp(),
+  s = i.requirejs("core");
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    results:[],
-    key:'',
+    results: [],
+    key: '',
   },
-  bindInput:function(e){
+  bindInput: function(e) {
     this.setData({
       key: e.detail.value
     })
   },
-  search:function(){
+  search: function() {
     var that = this;
-    if (that.data.key == ''){
+    if (that.data.key == '') {
       wx.showToast({
         title: '请先输入型号',
-        icon:'none'
+        icon: 'none'
       })
       return
     }
+    that.getData();  
+  },
+
+  getData() {
     wx.showLoading({
       title: '加载中',
     })
+    var that = this;
     s.get("merch/select_cate_get_goods", {
       'keyword': that.data.key
-    }, function (t) {
-        wx.hideLoading();
-        console.log(t)
+    }, function(t) {
+      wx.hideLoading();
+      if (t.code == 0) {
+        wx.showToast({
+          title: '暂无数据',
+          icon:'none'
+        })  
+      } else {
+        console.log(t.list)
+        that.setData({
+          results: t.list
+        })
+      }
     });
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    s.get("merch/select_cate_get_goods", {
-      'keyword':'GLA200'
-    }, function (t) {
-      
-    });
+  onLoad: function(options) {
+    this.getData();
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
