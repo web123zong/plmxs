bbde508eac5e54651ca905676c7f402f94b57d4d
limengge1314 <15626138260@163.com>
2019-09-07 14:31:49 +0800

    sort

--- store/pages/index/index.js
+++ store/pages/index/index.js
@@ -4,8 +4,7 @@ Page({
   data: {
     page: 1,
     isbottom: !1,
-    array: ['根据距离（默认）', '销量','评分'],
-    index: 0,
+    array: ['距离', '销量','评分'],
     chooseschool: 0,
     city: '',
     name: '',
@@ -14,10 +13,31 @@ Page({
     autoplay: true,
     interval: 5000,
     duration: 1000,
-    keyword:''
+    keyword:'',
+    sort:0,
+    showsortmodal:0,
+  },
+  showsort:function(){
+    if (this.data.showsortmodal == 0){
+      this.setData({
+        showsortmodal:1,
+      })
+    }else{
+      this.setData({
+        showsortmodal:0,
+      })
+    }
   },
   onPullDownRefresh: function () {
     var that = this;
+    that.setData({
+      list:'',
+      sort:0,
+      showsortmodal: 0,
+      list: [],
+      page:1,
+      isbottom:false,
+    })
     that.getmerch();
   },
   onLoad: function(a) {
@@ -34,21 +54,43 @@ Page({
       }
     })
   },
+  gosearch:function(e){
+    this.setData({
+      page:1,
+      sort: e.currentTarget.dataset.index,
+      list:[],
+      isbottom: false,
+    })
+    this.getmerch();
+  },
   serchinput:function(e){
-    console.log(e.detail.value)
     this.setData({
       keyword:e.detail.value
     })
   },
   serch:function(){
     var that = this;
+    that.setData({
+      sort:0,
+      showsortmodal: 0,
+      list: [],
+      page: 1,
+      isbottom: false,  
+    })
     e.get("merch/ajaxmerchuser", {
       'keyword': that.data.keyword,
       'sorttype': that.data.lng
     }, function (e) {
-      that.setData({
-        list: e.result.list
-      })
+      if (e.result.list > 0){
+        that.setData({
+          list: e.result.list
+        }) 
+      }else{
+        that.setData({
+          isbottom:true
+        })
+      }
+     
     });
   },
   onReady: function() {},
@@ -63,14 +105,24 @@ Page({
     e.get("merch/ajaxmerchuser", {
       'lat': that.data.lat,
       'lng': that.data.lng,
-      'page': that.data.page
+      'page': that.data.page,
+      'sort':that.data.sort,
+      'keyword': that.data.keyword,
     }, function(e) {
       wx.stopPullDownRefresh();
-      that.setData({
-        list: that.data.list.concat(e.result.list)
-      })
+      if(e.result.list.length > 0){
+        that.setData({
+          list: that.data.list.concat(e.result.list),
+          showsortmodal: 0,
+        })
+      }else{
+      console.log(111)
+        that.setData({
+          isbottom:true
+        })
+      }
     });
-    var page = e.page += 1;
+    var page = that.data.page + 1;
     that.setData({
       page: page
     })
